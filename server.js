c0cbb78592b628746a2d168fd25f3a03cc3fe44d
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-06-22 11:27:16 +0530

    update

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-import express from "express"
-const app = express()
-import ContactRoutes from "./routes/contacts.routes.js"
-import { connectDB } from "./config/database.js"
+import express from "express";
+import dotenv from "dotenv"; 
 
-const PORT = process.env.PORT
+import ContactRoutes from "./routes/contacts.routes.js";
+import { connectDB } from "./config/database.js";
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
 // Database Connection
-connectDB()
+dotenv.config();             
+connectDB();
+
+const app = express();
 
 // Middleware
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 // Routes
-app.use("/",ContactRoutes)
+app.use("/", ContactRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server started Successfully on port ${PORT}.`)
-})
+  console.log(`Server started successfully on port ${PORT}.`);
+});
